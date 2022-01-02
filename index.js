29c3bba6295925c1e6944d8e9b15b1bfd0c673dd
jarjug <jarjug@berea.edu>
2022-01-01 22:34:37 -0500

    Inititial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+// var person = {
+//   name: "Gaston",
+//   age : "24",
+//   address: {
+//     street: "101 Chestnut Street",
+//     city: "Berea"
+//   },
+//   children: ["Liberty","Ebe"]
+//
+//   }
+//
+//   var friends = [
+//     {
+//       name: "Liberty",
+//       hobby: "Music"
+//     },
+//     {
+//       name: "Ebe",
+//       hobby: "Matching with Wuranyaas"
+//     }
+//
+//   ]
+//
+//
+// person = JSON.stringify(person);
+// person = JSON.parse(person);
+//
+// friends = JSON.stringify(friends)
+// friends = JSON.parse(friends)
+//
+//
+// console.log(person.address.city);
+// console.log(friends[0].name)
+
+//
+// alert(3+7);
+// alert("I love you");
+
+// Creating Variables
+// var firstNumber = "3";
+// var secondNumber = 5;
+// var total = Number(firstNumber) + Number(secondNumber) // The Number functions converts other data types to an integer
+// alert(total);
+
+// Asking for user's input
+// var firstNumber = prompt("Enter the first number:");
+// var secondNumber = prompt("Enter the second number:");
+// var total = Number(firstNumber) + Number(secondNumber); // The Number functions converts other data types to an integer
+// alert(total);
+
+// const codingChallenge = 100;
+// alert(codingChallenge)
+
+// Writing a function
+// var personName = prompt("Enter your name:");
+// function codeSomething (){
+//   alert (personName + ", welcome to my first javaScript function code");
+// }
+
+
+// Writing an if-statement
+var userInput = prompt("Enter any number:")
+var myNumber = 10;
+if(userInput < myNumber){
+  alert(userInput + " is less than " + myNumber)
+}else{
+  alert(userInput + " is greater than " + myNumber)
+}
---
+++ script.js
@@ -0,0 +1,51 @@
+const showBtn = document.querySelector('.navBtn');
+const topNav = document.querySelector('.top-nav');
+
+showBtn.addEventListener('click', function(){
+    if(topNav.classList.contains('showNav')){
+        topNav.classList.remove('showNav');
+        showBtn.innerHTML = '<i class = "fas fa-bars"></i>';
+    } else {
+        topNav.classList.add('showNav');
+        showBtn.innerHTML = '<i class = "fas fa-times"></i>';
+    }
+});
+
+// /* Lightbox */
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+
+
+
+
+/* Start Contact */
+//Contact Form in PHP
+const form = document.querySelector("form"),
+statusTxt = form.querySelector(".button-area");
+form.onsubmit = (e)=>{
+  e.preventDefault();
+  statusTxt.style.color = "#0D6EFD";
+  statusTxt.style.display = "block";
+  statusTxt.innerText = "Sending your message...";
+  form.classList.add("disabled");
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "contact.php", true);
+  xhr.onload = ()=>{
+    if(xhr.readyState == 4 && xhr.status == 200){
+      let response = xhr.response;
+      if(response.indexOf("required") != -1 || response.indexOf("valid") != -1 || response.indexOf("failed") != -1){
+        statusTxt.style.color = "red";
+      }else{
+        form.reset();
+        setTimeout(()=>{
+          statusTxt.style.display = "none";
+        }, 3000);
+      }
+      statusTxt.innerText = response;
+      form.classList.remove("disabled");
+    }
+  }
+  let formData = new FormData(form);
+  xhr.send(formData);
+}
+/* End Contact */
